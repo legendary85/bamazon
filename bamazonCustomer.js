3d4da8ac1bdc0e8ecc78bf8cbe3746a6d2b2a5bd
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-08-30 17:17:27 -0500

    successfully  display show all contents.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,44 +14,74 @@ connection.connect(function(err) {
   if (err) throw err;
   runSearch();
 });
+
 function runSearch() {
   console.log("Connected as id: " + connection.threadId);
-  // inquirer;
-  // .prompt({
-  //   name: "action",
-  //   type: "rawlist",
-  //   message: "What would you like to do?",
-  //   choices: [
-  //     "Find songs by artist",
-  //     "Find all artists who appear more than once",
-  //     "Find data with a specific range",
-  //     "Find for a specific song",
-  //     "Find artists with a top song and top album in the same year"
-  //   ]
-  // })
-  // .then(function(answer) {
-  //   //switch case
-  //   switch (answer.action) {
-  //     case "Find songs by artist":
-  //       artistSearch();
-  //       break;
+  console.log("Welcome to Bamazon!");
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "Please make a selection",
+      choices: [
+        "Show all items",
+        "Find all artists who appear more than once",
+        "Find data with a specific range",
+        "Find for a specific song",
+        "Find artists with a top song and top album in the same year"
+      ]
+    })
+    .then(function(answer) {
+      //switch case
+      switch (answer.action) {
+        case "Show all items":
+          showAllItems();
+          break;
 
-  //     case "Find all artists who appear more than once":
-  //       multiSearch();
-  //       break;
+        case "Find all artists who appear more than once":
+          multiSearch();
+          break;
 
-  //     case "Find data with a specific range":
-  //       rangeSearch();
-  //       break;
+        case "Find data with a specific range":
+          rangeSearch();
+          break;
 
-  //     case "Find for a specific song":
-  //       songSearch();
-  //       break;
+        case "Find for a specific song":
+          songSearch();
+          break;
 
-  //     case "Find artists with a top song and top album in the same year":
-  //       songAndAlbumSearch();
-  //       break;
-  //   }
-  // });
+        case "Find artists with a top song and top album in the same year":
+          songAndAlbumSearch();
+          break;
+      }
+    });
   //run in terminal to make sure there's a connection (node GreatBay.js)
+}
+
+function showAllItems() {
+  // // connection.connect();
+  var contentQuery = "SELECT * FROM products;";
+  connection.query(contentQuery, function(err, results) {
+    if (err) {
+      console.log("A error has occured.");
+      console.log(err);
+    }
+    var contentArray = [];
+    for (var i = 0; i < results.length; i++) {
+      contentArray.push(results[i]);
+      // console.log(contentArray);
+      console.log(
+        "\n" +
+          "ID: " +
+          results[i].item_id +
+          "\nName: " +
+          results[i].product_name +
+          "\nPrice: " +
+          "$" +
+          results[i].price +
+          "\n=================\n"
+      );
+    }
+    runSearch();
+  });
 }
